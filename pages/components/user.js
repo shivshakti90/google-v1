07d751fddce6ea74e18ae31d7cf446786180ff16
unknown <shivshakti90@gmail.com>
2022-12-30 14:37:10 +0530

    create new header section

---
+++ pages/components/user.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function User() {
+  return (
+    <div className="w-8 rounded-full overflow-hidden">
+      <img src="./images/profilePic.jpeg" />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Header from './components/header.js'
+import Body from './components/body.js'
+import Footer from './components/footer.js'
 export default function Home() {
   return (
     <>
@@ -14,7 +11,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hellow World!</h1>
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      <Body />
+      {/* Footer */}
+      <Footer />
     </>
   )
 }
